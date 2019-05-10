544a0ffb113c61f73f452e08152c4e92d28f934c
shido94 <rupeshyadav94.ry@gmail.com>
2019-05-11 01:02:34 +0530

    new commit

---
+++ routes/defaultRoute.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const route = express.Router();
+const ClubList = require('../src/models/collageClub');
+const { User } = require('../src/models/user');
+const ClubAchievement = require('../src/models/clubAchievement');
+const ClubDetails = require('../src/models/clubDetails');
+const ClubMedia = require('../src/models/clubMedia');
+
+route.get('/', async (req, res) => {
+    try {
+        let club_data = await ClubList.aggregate([{
+            "$lookup": {
+                from: "clubdetails",
+                localField: "_id",
+                foreignField: "club_id",
+                as: "clubDetails",
+            }
+        }, {
+            "$sort": {
+                "created_at": -1
+            }
+        }
+        ]);
+        if (club_data.length !== 0) {
+            res.render('default/index', { club_data });
+        } else {
+            res.render('default/index', { club_data:undefined });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+route.get('/fckietclub', (req,res) => {
+    res.render('default/fckiet');
+});
+
+route.get('/kommotion', (req,res) => {
+    res.render('default/kommotion');
+});
+
+route.get('/phoenix', (req,res) => {
+    res.render('default/pheonix');
+});
+
+route.get('/steppers', (req,res) => {
+    res.render('default/steppers');
+});
+
+route.get('/ecell', (req,res) => {
+    res.render('default/ecell');
+});
+
+route.get('/uddeshhya', (req,res) => {
+    res.render('default/uddeshya');
+});
+
+route.get('/rann', (req,res) => {
+    res.render('default/rann');
+});
+
+route.get('/innogeeks', (req,res) => {
+    res.render('default/innogeeks');
+});
+
+route.get('/innotech', (req,res) => {
+    res.render('default/innotech');
+});
+
+route.get('/endeavour', (req,res) => {
+    res.render('default/endeavour');
+});
+
+route.get('/epoque', (req,res) => {
+    res.render('default/epoque');
+});
+
+route.get('/prastuti', (req,res) => {
+    res.render('default/prastuti');
+});
+
+// route.get('/club/:club_name', async (req, res) => {
+//     try {
+//         let club_name = req.params.club_name;
+//         let club_name_str = club_name.split('_').join(' ');
+//         let club_id = await ClubList.find({ club_name: club_name_str});
+//         let club_data =await ClubDetails.find({club_id:club_id[0]._id});
+//         let club_media_data =await ClubMedia.find({club_id:club_id[0]._id});
+//         // res.json(
+//         //     {
+//         //         club_data,
+//         //         club_id,
+//         // club_media_data
+//         //     }
+//         // );
+//         if (club_data.length !== 0) {
+//             res.render('default/about', { club_data, club_id, club_media_data });
+//         } else {
+//             res.send(404);
+//         }
+//         // res.render('default/about');
+//     } catch (error) {
+//         console.error(error);
+//     }
+// });
+
+route.get('/contact', async (req, res) => {
+    res.render('default/contact');
+});
+route.get('/signup', (req,res) => {
+    res.render('common/signup');
+});
+module.exports = route;
---
+++ src/models/user.js
@@ -0,0 +1,116 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        require: true
+    },
+    username: {
+        type: String,
+        require: true
+    },
+    password_hash: {
+        type: String,
+        require: true
+    },
+    password_reset_token: {
+        type: String,
+        require: false,
+        default: null
+    },
+    userLevel: {
+        type: Number,
+        require: true,
+        default: 0
+    },
+    userType: {
+        type: String,
+        require: true,
+        default: "student"
+    },
+    college_id: {
+        type: String,
+        default: null
+    },
+    college: {
+        type: String,
+        require: true,
+        default: "KIET"
+    },
+    year: {
+        type: Number,
+        min: 1,
+        max: 4,
+        required: true
+    },
+    branch: {
+        type: String,
+        require: true
+    },
+    session_time: {
+        type: Number,
+        require: true,
+        default: 0
+    },
+    updatedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: false,
+        default: null
+    },
+    created_at: {
+        type: Date,
+        default: Date.now(),
+    },
+    updated_at: {
+        type: Date,
+        default: null
+    }
+});
+
+userSchema.statics.Finduser = async function(data) {
+    // console.log(data);
+    return await this.model('User').findOne({
+        "$or": [
+            {
+                email: data
+            }, {
+                college_id: data
+            }
+        ]
+    });
+};
+
+userSchema.pre('save', function (next) {
+    var user = this;
+    var SALT_WORK_FACTOR = 10;
+    if (!user.isModified('password_hash')) return next();
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password_hash, salt, function (err, hash) {
+            if (err) return next(err);
+            user.password_hash = hash;
+            next();
+        });
+    });
+});
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    console.log(candidatePassword);
+    var isMatch = await bcrypt.compare(candidatePassword, this.password_hash);
+    return isMatch;
+};
+
+var User = mongoose.model('User', userSchema);
+
+User.create({
+    email: "goelaniruddh@gmail.com",
+    username: "devil",
+    password_hash: "bijnor12",
+    userLevel: 2,
+    college_id:'1519it1018',
+    year: '4',
+    branch: "IT"
+});
+
+module.exports = { User };
